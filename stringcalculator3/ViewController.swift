afa5fa8c3de1c666757bc4de6a1202a261ad1748
Fernando <markussen145@gmail.com>
2019-12-11 13:30:52 +0100

    Initial Commit

---
+++ stringcalculator3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stringcalculator3
+//
+//  Created by Fernando Gil Otero on 11/12/2019.
+//  Copyright © 2019 Fernando Gil Otero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ stringcalculator3Tests/stringcalculator3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  stringcalculator3Tests.swift
+//  stringcalculator3Tests
+//
+//  Created by Fernando Gil Otero on 11/12/2019.
+//  Copyright © 2019 Fernando Gil Otero. All rights reserved.
+//
+
+import XCTest
+@testable import stringcalculator3
+
+class stringcalculator3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
